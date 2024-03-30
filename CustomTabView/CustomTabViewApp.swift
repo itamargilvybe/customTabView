325a6d337a05f7fd17602c83717a4baf4eb8e3e7
itamargilvybe <108910701+itamargilvybe@users.noreply.github.com>
2024-03-30 18:22:35 +0300

    Initial Commit

---
+++ CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by Itamar Gil on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
